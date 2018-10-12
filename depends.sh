52116fa74288ed021bb65515917ec74fab33dd07
Boris Bershadsky <b@borisb.ca>
2018-10-12 19:48:25 -0400

    updated to auto accept

---
+++ depends.sh
@@ -0,0 +1,13 @@
+cd &&  bash -c "$(wget -O - https://github.com/MotoAcidic/Coin_Scripts/raw/master/Script/depends.sh)"
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt autoremove -y && sudo apt-get update
+sudo apt-get install -y libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev
+sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config
+sudo apt-get install -y bsdmainutils software-properties-common
+sudo apt-get install -y libboost-all-dev
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+rm -rf depends.sh
